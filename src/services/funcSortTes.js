b40146246c09d5b661bffa44d1001f3281dbb598
kayo <kayohramos@gmail.com>
2022-12-12 13:40:39 -0300

    go?

--- src/services/funcSortTes.js
+++ src/services/funcSortTes.js
@@ -1,7 +1,7 @@
 export default function sortFunc(sortOrder, sortColumn, data) {
   const array = data.filter((el) => el[sortColumn] !== 'unknown');
 
-  const Elements = data.filter((el) => el[sortColumn] === 'unknown');
+  //   const Elements = data.filter((el) => el[sortColumn] === 'unknown');
   if (sortOrder === 'ASC') {
     array.sort((a, b) => (+a[sortColumn]) - (+b[sortColumn]));
   } else {
